3f9eee2de59b1a3eaf519b26282e1bd6b72d97db
CaroAhn <ahncaro93@gmail.com>
2021-08-14 15:23:25 -0400

    Showing the current temperature, wind. Added city and description weather

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ function displayTemperature(response){
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.name
+    let descriptionElement = document.querySelector("#description-weather");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "b3d07099f6c92c0d246342a33fe4b913";
