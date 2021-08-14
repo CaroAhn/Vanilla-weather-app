b2d614383865a9cdf0893c3fa82f32ef46203a03
CaroAhn <ahncaro93@gmail.com>
2021-08-14 15:51:48 -0400

    Added new date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,40 @@
+let now = new Date();
+let h1 = document.querySelector("h1");
+let date = now.getDate();
+let hour = now.getHours();
+let minute = now.getMinutes();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+h1.innerHTML = `${hour}:${minute}
+</br>
+${day}, ${month} ${date}`;
+
 function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
