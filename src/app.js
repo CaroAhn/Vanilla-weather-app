2ef4888c25f620beaf5e7ab40ccd273be891f0de
CaroAhn <ahncaro93@gmail.com>
2021-08-14 13:17:59 -0400

    Added JS API

---
+++ src/app.js
@@ -0,0 +1,11 @@
+function displayTemperature(response){
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.name
+}
+
+let apiKey = "b3d07099f6c92c0d246342a33fe4b913";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
